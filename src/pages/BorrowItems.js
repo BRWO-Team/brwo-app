5f3cc561298de0e582ef18418cbaae2ac61b13c7
unknown <taylor.alan.johnson@gmail.com>
2019-12-14 21:51:45 -0500

    get firebase set up

--- src/pages/BorrowItems.js
+++ src/pages/BorrowItems.js
@@ -1,11 +1,52 @@
 import React from "react";
-import ItemsList from '../components/ItemsList'
+import ItemsList from "../components/ItemsList";
+const firebase = require("firebase");
+// const admin = require("firebase-admin");
 
 class BorrowItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD8D423kx8cOCiGiLOTx-L1i5INEoFYAcI",
+      authDomain: "pacific-plating-262123.firebaseapp.com",
+      projectId: "pacific-plating-262123"
+    });
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("tayloraj10@gmail.com", "kipper100")
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+
+    let db = firebase.firestore();
+
+    db.collection("items")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          this.setState({ data: this.state.data.concat(doc.data()) });
+          console.log(this.state)
+        });
+      });
+  }
+
   render() {
+    const listItems = this.state.data.map((item) =>
+        <li key={item.id}>{item.title}</li> 
+    );
     return (
       <div className="borrowItems">
-        <h1>Hi, this is a test page for borrowing items</h1>
+        <ul>
+          {listItems}
+        </ul>
         <ItemsList />
       </div>
     );
