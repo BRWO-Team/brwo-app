7a72afb62b7f4883c252bb8458362d435862e1e5
unknown <taylor.alan.johnson@gmail.com>
2020-01-09 22:52:59 -0500

    continue work on add form, can now submit

--- src/components/AddFormControl.js
+++ src/components/AddFormControl.js
@@ -7,47 +7,175 @@ import { changeRoute } from '../actions/router.action';
 class AddFormControl extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { type: 'listing' };
+    this.state = {
+      listingType: 'listing',
+      itemType: '',
+      title: '',
+      description: '',
+      categories: [],
+      price: 0,
+      images: [],
+      latitude: '',
+      longitude: '',
+      zipCode: '',
+      userId: '',
+      date_time_added: ''
+    };
     this.listingChanged = this.listingChanged.bind(this);
     this.requestChanged = this.requestChanged.bind(this);
+    this.captureData = this.captureData.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  captureData(inputType, data) {
+    alert(inputType);
+    switch (inputType) {
+      case 'title':
+        this.setState({ title: data });
+        break;
+    }
   }
 
   listingChanged() {
     this.setState({
-      type: 'listing'
+      listingType: 'listing'
     });
   }
 
   requestChanged() {
     this.setState({
-      type: 'requesting'
+      listingType: 'requesting'
     });
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    // alert('You are submitting');
+    console.log(event);
+
+    fetch(
+      'https://cors-anywhere.herokuapp.com/https://api-dot-pacific-plating-262123.appspot.com/api/v1.0/items/categories',
+      {
+        method: 'GET'
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        console.log(jsonData);
+      });
+
+    fetch(
+      'https://cors-anywhere.herokuapp.com/https://api-dot-pacific-plating-262123.appspot.com/api/v1.0/items/postnew',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          listingType: this.state.listingType,
+          itemType: this.state.itemType,
+          title: this.state.title,
+          description: this.state.description,
+          price: this.state.price,
+          date_time_added: Date(),
+          test: 'yes'
+        })
+      }
+    );
+  };
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const id = target.id;
+    const name = target.name;
+    console.log('value: ' + value);
+    console.log('id: ' + id);
+    console.log('name: ' + name);
+
+    switch (id) {
+      case 'item-radio':
+        this.setState({ [name]: 'item' }, () => {
+          console.log(this.state);
+        });
+        break;
+      case 'time-radio':
+        this.setState({ [name]: 'time' }, () => {
+          console.log(this.state);
+        });
+        break;
+      case 'requesting-radio':
+        this.setState({ [name]: 'requesting' }, () => {
+          console.log(this.state);
+        });
+        break;
+      case 'listing-radio':
+        this.setState({ [name]: 'listing' }, () => {
+          console.log(this.state);
+        });
+        break;
+      case 'title':
+        this.setState({ [name]: 'test' }, () => {
+          console.log(this.state);
+        });
+        break;
+      default:
+        this.setState({ [name]: value }, () => {
+          console.log(this.state);
+        });
+        break;
+    }
+
+    // this.setState({
+    //   [name]: value
+    // });
+    // console.log(this.state);
   }
 
   render() {
     let type;
-    if (this.state.type == 'listing') {
-      type = <AddFormListing type={this.state.type} />;
-    } else if (this.state.type == 'requesting') {
-      type = <AddFormRequest type={this.state.type} />;
+    if (this.state.listingType == 'listing') {
+      type = (
+        <AddFormListing
+          type={this.state.listingType}
+          handleChange={this.handleInputChange}
+        />
+      );
+    } else if (this.state.listingType == 'requesting') {
+      type = (
+        <h1>This will be the listing form</h1>
+        // <AddFormRequest
+        //   type={this.state.listingType}
+        //   handleChange={this.handleInputChange}
+        // />
+      );
     }
 
     return (
       <div className='add-form-control'>
-        <div className='container'>
-          <div className='row'>
-            <div>
-              <h2>Item or Time</h2>
-              <form>
+        <form onSubmit={this.submitHandler} method='POST'>
+          <div className='container add-form-item'>
+            <div className='row'>
+              <div>
+                <h2>Item or Time</h2>
+
                 <div className='form-group'>
                   <div className='custom-control custom-radio mb-2'>
                     <input
                       type='radio'
                       id='item-radio'
-                      name='custom-radio'
                       className='custom-control-input'
+                      onChange={this.handleInputChange}
+                      name='itemType'
                     />
-                    <label className='custom-control-label' for='item-radio'>
+                    <label
+                      className='custom-control-label'
+                      htmlFor='item-radio'
+                    >
                       Item
                     </label>
                   </div>
@@ -55,58 +183,65 @@ class AddFormControl extends React.Component {
                     <input
                       type='radio'
                       id='time-radio'
-                      name='custom-radio'
                       className='custom-control-input'
+                      onChange={this.handleInputChange}
+                      name='itemType'
                     />
-                    <label className='custom-control-label' for='time-radio'>
+                    <label
+                      className='custom-control-label'
+                      htmlFor='time-radio'
+                    >
                       Time
                     </label>
                   </div>
                 </div>
-              </form>
+              </div>
             </div>
-          </div>
-          <div className='row'>
-            <div>
-              <h2>List or Request</h2>
-              <form>
+            <div className='row'>
+              <div>
+                <h2>List or Request</h2>
                 <div className='form-group'>
                   <div className='custom-control custom-radio mb-2'>
                     <input
-                      onChange={this.listingChanged}
+                      // onChange={this.listingChanged}
                       type='radio'
-                      id='custom-radio-example-1'
-                      name='custom-radio'
+                      id='listing-radio'
                       className='custom-control-input'
+                      onChange={this.handleInputChange}
+                      name='listingType'
                     />
                     <label
                       className='custom-control-label'
-                      for='custom-radio-example-1'
+                      htmlFor='listing-radio'
                     >
                       Listing
                     </label>
                   </div>
                   <div className='custom-control custom-radio'>
                     <input
-                      onChange={this.requestChanged}
+                      // onChange={this.requestChanged}
                       type='radio'
-                      id='custom-radio-2'
-                      name='custom-radio'
+                      id='requesting-radio'
                       className='custom-control-input'
+                      onChange={this.handleInputChange}
+                      name='listingType'
                     />
                     <label
                       className='custom-control-label'
-                      for='custom-radio-2'
+                      htmlFor='requesting-radio'
                     >
                       Requesting
                     </label>
                   </div>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
-        </div>
-        {type}
+          {type}
+          <button type='submit' className='btn btn-primary back-btn'>
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
--- src/components/AddFormListing.js
+++ src/components/AddFormListing.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import ImageUploader from 'react-images-upload';
 
 class AddFormListing extends React.Component {
   render() {
     return (
-      <div className='add-form'>
-        <div className='container'>
+      <div className='add-form add-form-item'>
+        <div className='container '>
           <div className='row'>
             <div>
               <h2>Title</h2>
-              <form>
-                <div className='form-group'>
-                  <input
-                    type='text'
-                    className='form-control add-title'
-                    id='input-group-example-std'
-                    aria-describedby='input-group-example-std'
-                    placeholder={'What are you ' + this.props.type + '?'}
-                  />
-                </div>
-              </form>
+              <div className='form-group'>
+                <input
+                  type='text'
+                  className='form-control add-title'
+                  id='input-group-example-std'
+                  aria-describedby='input-group-example-std'
+                  placeholder={'What are you ' + this.props.type + '?'}
+                  onBlur={this.props.handleChange}
+                  name='title'
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -27,19 +28,19 @@ class AddFormListing extends React.Component {
           <div className='row'>
             <div>
               <h2>Description</h2>
-              <form>
-                <div className='form-group'>
-                  <label for='textarea-example-1'>
-                    {'Please provide a brief description of what you are ' +
-                      this.props.type}
-                  </label>
-                  <textarea
-                    className='form-control'
-                    id='textarea-example-1'
-                    rows='3'
-                  ></textarea>
-                </div>
-              </form>
+              <div className='form-group'>
+                <label htmlFor='textarea-example-1'>
+                  {'Please provide a brief description of what you are ' +
+                    this.props.type}
+                </label>
+                <textarea
+                  className='form-control'
+                  id='textarea-example-1'
+                  rows='3'
+                  onBlur={this.props.handleChange}
+                  name='description'
+                ></textarea>
+              </div>
             </div>
           </div>
         </div>
@@ -48,48 +49,46 @@ class AddFormListing extends React.Component {
           <div className='row'>
             <div>
               <h2>Categories</h2>
-              <form>
-                <div className='custom-control custom-switch add-form-category-checkbox'>
-                  <input
-                    type='checkbox'
-                    className='custom-control-input'
-                    id='customSwitch1'
-                  />
-                  <label className='custom-control-label' for='customSwitch1'>
-                    Book
-                  </label>
-                </div>
-                <div className='custom-control custom-switch add-form-category-checkbox'>
-                  <input
-                    type='checkbox'
-                    className='custom-control-input'
-                    id='customSwitch2'
-                  />
-                  <label className='custom-control-label' for='customSwitch2'>
-                    Sports Equipment
-                  </label>
-                </div>
-                <div className='custom-control custom-switch add-form-category-checkbox'>
-                  <input
-                    type='checkbox'
-                    className='custom-control-input'
-                    id='customSwitch3'
-                  />
-                  <label className='custom-control-label' for='customSwitch3'>
-                    Tool
-                  </label>
-                </div>
-                <div className='custom-control custom-switch add-form-category-checkbox'>
-                  <input
-                    type='checkbox'
-                    className='custom-control-input'
-                    id='customSwitch4'
-                  />
-                  <label className='custom-control-label' for='customSwitch4'>
-                    Miscellaneous
-                  </label>
-                </div>
-              </form>
+              <div className='custom-control custom-switch add-form-category-checkbox'>
+                <input
+                  type='checkbox'
+                  className='custom-control-input'
+                  id='customSwitch1'
+                />
+                <label className='custom-control-label' htmlFor='customSwitch1'>
+                  Book
+                </label>
+              </div>
+              <div className='custom-control custom-switch add-form-category-checkbox'>
+                <input
+                  type='checkbox'
+                  className='custom-control-input'
+                  id='customSwitch2'
+                />
+                <label className='custom-control-label' htmlFor='customSwitch2'>
+                  Sports Equipment
+                </label>
+              </div>
+              <div className='custom-control custom-switch add-form-category-checkbox'>
+                <input
+                  type='checkbox'
+                  className='custom-control-input'
+                  id='customSwitch3'
+                />
+                <label className='custom-control-label' htmlFor='customSwitch3'>
+                  Tool
+                </label>
+              </div>
+              <div className='custom-control custom-switch add-form-category-checkbox'>
+                <input
+                  type='checkbox'
+                  className='custom-control-input'
+                  id='customSwitch4'
+                />
+                <label className='custom-control-label' htmlFor='customSwitch4'>
+                  Miscellaneous
+                </label>
+              </div>
             </div>
           </div>
         </div>
@@ -98,19 +97,33 @@ class AddFormListing extends React.Component {
           <div className='row'>
             <div>
               <h2>Price</h2>
-              <form>
-                <div className='input-group mb-3'>
-                  <div className='input-group-prepend'>
-                    <span className='input-group-text'>$</span>
-                  </div>
-                  <input
-                    type='text'
-                    className='form-control'
-                    aria-label='Amount (to the nearest dollar)'
-                  />
+              <div className='input-group mb-3'>
+                <div className='input-group-prepend'>
+                  <span className='input-group-text'>$</span>
                 </div>
-              </form>
+                <input
+                  type='text'
+                  className='form-control'
+                  aria-label='Amount (to the nearest dollar)'
+                  onBlur={this.props.handleChange}
+                  name='price'
+                />
+              </div>
             </div>
+          </div>
+        </div>
+
+        <div className='container'>
+          <div className='row'>
+            <h2>Add Images</h2>
+            <ImageUploader
+              withIcon={false}
+              buttonText='Choose images'
+              onChange={this.onDrop}
+              imgExtension={['.jpg', '.gif', '.png']}
+              maxFileSize={5242880}
+              withPreview={true}
+            />
           </div>
         </div>
       </div>
